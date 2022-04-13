ca8fe0cf4a14dfcd64b100c7c05466190548df7c
Elxan <elxan.civishov2000@gmail.com>
2022-04-13 09:47:48 +0400

    update

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
 let btn = document.querySelectorAll("button");
 let data = [];
 let ul = document.querySelector("ul");
-let count = 0;
+
+//button click event 
 btn.forEach((item) => {
   item.addEventListener("click", (event) => {
-    count++;
-
     let input = document.querySelector("#textInput");
+    let error = document.querySelector(".error");
+    input.addEventListener("change", (event) => {
+      error.style.display = "none";
+    });
+
     if (input.value != "") {
       let li = document.createElement("li");
       li.innerText = input.value;
       //data
       data[length] = input.value;
       console.log(data[length]);
-
       console.log(li);
       ul.append(li);
     } else {
-      console.log(alert("Pleace enter the input value ."));
+      error.style.display = "block";
     }
     let div = document.querySelector(".insert-list");
-
     if ((ul.childElementCount = 0)) {
       div.style.display = "none";
     } else {
       div.style.display = "block";
-    }
-
-    let divInput = document.querySelector(".textInput-div");
-    if (divInput.style.display === "block") {
-      divInput.style.display = "none";
-    } else {
-      divInput.style.display = "block";
     }
     event.preventDefault();
   });
